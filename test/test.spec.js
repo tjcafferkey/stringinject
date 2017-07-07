4acfcf98a94fd51e502afb914e818f8fe1ceed9e
Tom Cafferky <tjcafferkey@gmail.com>
2017-07-07 23:06:31 +0100

    Unit tests

--- test/test.spec.js
+++ test/test.spec.js
@@ -3,6 +3,33 @@ var expect = require("chai").expect;
 var assert = require("chai").assert;
 var stringInject = require("../lib/index").default;
 
-describe("test", function() {
+describe("replace brackets with array items", function() {
+    it("should replace brackets {0} in string with array[0]", function() {
+        var str = stringInject("My username is {0}", ["tjcafferkey"]);
+        expect(str).to.equal("My username is tjcafferkey");
+    });
 
+    it("should replace brackets {0} and {1} in string with array[0] and array[1]", function() {
+        var str = stringInject("I am {0} the {1} function", ["testing", "stringInject"]);
+        expect(str).to.equal("I am testing the stringInject function");
+    });
+});
+
+describe("pass in a string with no {} with an array of items", function() {
+    it("should return the same string as passed in", function() {
+        var str = stringInject("This should be the same", ["testing", "stringInject"]);
+        expect(str).to.equal("This should be the same");
+    });
+});
+
+describe("pass in incorrect parameters", function() {
+    it("should return false when passed a number instead of a string as first parameter", function() {
+        var str = stringInject(100, ["testing", "stringInject"]);
+        expect(str).to.be.false;
+    });
+
+    it("should return false when passed a number instead of an array as second parameter", function() {
+        var str = stringInject("hello", 1);
+        expect(str).to.be.false;
+    });
 });
