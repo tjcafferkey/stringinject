8479e05a4db839f05fd393e793e4d855908df6a4
Jason Haines <jason.haines@versent.com.au>
2019-06-27 13:47:34 +1000

    Refactor replace regexes
    Use arrow-functions and regex-group match to simplify code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 export default function stringInject(str, data) {
     if (typeof str === 'string' && (data instanceof Array)) {
 
-        return str.replace(/({\d})/g, function(i) {
-            return data[i.replace(/{/, '').replace(/}/, '')];
-        });
+        return str.replace(/{(\d)}/g,
+            ( match, index ) => data[index]
+        );
     } else if (typeof str === 'string' && (data instanceof Object)) {
 
         if (Object.keys(data).length === 0) {
@@ -11,14 +11,9 @@ export default function stringInject(str, data) {
         }
 
         for (let key in data) {
-            return str.replace(/({([^}]+)})/g, function(i) {
-                let key = i.replace(/{/, '').replace(/}/, '');
-                if (!data[key]) {
-                    return i;
-                }
-
-                return data[key];
-            });
+            return str.replace(/{([^}]+)}/g,
+                ( match, key ) => ( data[key] ? data[key] : match )
+            );
         }
     } else if (typeof str === 'string' && data instanceof Array === false || typeof str === 'string' && data instanceof Object === false) {
 
